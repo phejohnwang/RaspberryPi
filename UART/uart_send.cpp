e8627f1770aec25be503010a6c7991963e544dac
phejohnwang <snswordsman@163.com>
2016-04-14 20:20:42 -0400

    Create uart_send.cpp

---
+++ UART/uart_send.cpp
@@ -0,0 +1,32 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int fd;
+	char buf[1024] = "Pheno!";
+
+	//Initialization
+	if (wiringPiSetup() < 0)
+		perror("WiringPiSetup problem \n ");	
+		
+	if ((fd = serialOpen("/dev/ttyAMA0", 57600)) < 0)
+		//57600 is the Baud used
+		perror("device not opened \n");
+
+	while (true) {
+		cin >> buf;
+		serialPuts(fd, buf); //Send message
+		printf("Sent Data: %s\n", buf);
+	}
+
+	return 0;
+}
